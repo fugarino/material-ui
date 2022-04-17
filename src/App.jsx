b00ecc8e0e40d2aff0860bf06e65fe2b953f4726
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-17 17:50:14 -0400

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone This is a photo album and I'm trying to make this
+              sentance as long as possible so we can see how it looks like on
+              the screen
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default App;
+<div>
+  <Typography variant="h1">Hello worlddd</Typography>
+</div>;
